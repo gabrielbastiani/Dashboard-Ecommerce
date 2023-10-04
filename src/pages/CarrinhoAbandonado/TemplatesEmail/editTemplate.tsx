22ef46b612653afec5259220363dc55c3136aa1b
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-10-03 22:32:55 -0300

    abandoned

--- src/pages/CarrinhoAbandonado/TemplatesEmail/editTemplate.tsx
+++ src/pages/CarrinhoAbandonado/TemplatesEmail/editTemplate.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
+import { Editor } from "@monaco-editor/react";
 import { setupAPIClient } from "../../../services/api";
 import { toast } from "react-toastify";
 import { Grid } from "../../Dashboard/styles";
 import MainHeader from "../../../components/MainHeader";
 import Aside from "../../../components/Aside";
-import { BlockTop, Container } from "../../Categorias/styles";
+import { Block, BlockTop, Container, Etiqueta } from "../../Categorias/styles";
 import { Card } from "../../../components/Content/styles";
 import Voltar from "../../../components/Voltar";
 import Titulos from "../../../components/Titulos";
@@ -21,24 +22,16 @@ const EditTemplate: React.FC = () => {
     let { templateAbandonedCartEmail_id } = useParams();
 
     const [name_file_email, setName_file_email] = useState("");
+    const [slugName, setSlugName] = useState("");
+    const [template, setTemplate] = useState<string>("");
+
+    console.log(template)
 
     const [modalVisible, setModalVisible] = useState(false);
 
-
-    useEffect(() => {
-        async function loadTemplates() {
-            try {
-                const apiClient = setupAPIClient();
-                const { data } = await apiClient.get(`/findUniqueTemplateEmailAbandonedCart?templateAbandonedCartEmail_id=${templateAbandonedCartEmail_id}`);
-
-                setName_file_email(data.name_file_email || "");
-
-            } catch (error) {/* @ts-ignore */
-                console.log(error.response.data);
-            }
-        }
-        loadTemplates();
-    }, [templateAbandonedCartEmail_id]);
+    function handleValueEditor(value: SetStateAction<string>, event: any) {
+        setTemplate(value)
+    }
 
     async function refreshConfig() {
         try {
@@ -49,6 +42,61 @@ const EditTemplate: React.FC = () => {
 
         } catch (error) {/* @ts-ignore */
             console.log(error.response.data);
+        }
+    }
+
+    useEffect(() => {
+        async function loadNameTemplates() {
+            try {
+                const apiClient = setupAPIClient();
+                const { data } = await apiClient.get(`/findUniqueTemplateEmailAbandonedCart?templateAbandonedCartEmail_id=${templateAbandonedCartEmail_id}`);
+
+                setName_file_email(data.name_file_email || "");
+                setSlugName(data.slug_name_file_email || "");
+
+            } catch (error) {/* @ts-ignore */
+                console.log(error.response.data);
+            }
+        }
+        loadNameTemplates();
+    }, [templateAbandonedCartEmail_id]);
+
+    useEffect(() => {
+        const loadTemplate = async () => {
+            try {
+                const response = await fetch(`/getTemplateEmailAbandoned?slug_name=${slugName}`);
+                if (!response.ok) {
+                    throw new Error('Falha ao buscar os dados da API');
+                }
+                const content = await response.text();
+                setTemplate(content);
+            } catch (erro) {
+                console.error('Erro ao buscar dados da API:', erro);
+            }
+        };
+        loadTemplate();
+    }, [slugName]);
+
+    async function updateContentTemplate() {
+        try {
+            const response = await fetch(`/updateFileTemplateAbandonedCart?slug_name=${slugName}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json', // Defina o tipo de conteúdo como JSON
+                },
+                body: JSON.stringify({
+                    content: template
+                }), // Converte os dados para JSON e envia no corpo
+            });
+
+            if (response.ok) {
+                console.log('Dados atualizados com sucesso');
+                refreshConfig();
+            } else {
+                console.error('Falha ao atualizar dados');
+            }
+        } catch (error) {/* @ts-ignore */
+            console.error('Erro ao fazer a solicitação:', error.response.data);
         }
     }
 
@@ -125,6 +173,29 @@ const EditTemplate: React.FC = () => {
                                 }
                             />
                         </BlockDados>
+
+                        <Block
+                            style={{ width: '100%' }}
+                        >
+                            <Etiqueta>Atualize abaixo o template do e-mail</Etiqueta>
+                            <br />
+                            <br />
+                            <br />
+                            <Button
+                                onClick={updateContentTemplate}
+                            >
+                                Atualizar
+                            </Button>
+                            <br />
+                            <Editor
+                                height="80vh"
+                                width="100%"
+                                theme="vs-dark"
+                                defaultLanguage="html"
+                                value={template}/* @ts-ignore */
+                                onChange={handleValueEditor}
+                            />
+                        </Block>
                     </Card>
                 </Container >
             </Grid >
